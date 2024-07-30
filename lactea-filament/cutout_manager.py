31a80c30d9c8a5ef1566dbedd18e43c8d970e165
Savannah Gramze <savannahgramze@login11.ufhpc>
2024-07-30 19:39:52 -0400

    spitzer cutouts

--- lactea-filament/cutout_manager.py
+++ lactea-filament/cutout_manager.py
@@ -2,6 +2,8 @@ import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.nddata import Cutout2D
+from astropy.visualization import simple_norm
+
 
 
 def get_cutout(filename, position, l, w, format='fits'):
@@ -64,3 +66,41 @@ def get_cutout_rgb(position, l, w):
     ]).swapaxes(0,2)
 
     return rgb_scaled.swapaxes(0,1), cutout_R.wcs
+
+def get_cutout_I1(position, l, w):
+    fn = '/orange/adamginsburg/cmz/glimpse_data/GLM_00000+0000_mosaic_I1.fits'
+    return get_cutout(fn, position, l, w)
+
+def get_cutout_I2(position, l, w):
+    fn = '/orange/adamginsburg/cmz/glimpse_data/GLM_00000+0000_mosaic_I2.fits'
+    return get_cutout(fn, position, l, w)
+
+def get_cutout_I3(position, l, w):
+    fn = '/orange/adamginsburg/cmz/glimpse_data/GLM_00000+0000_mosaic_I3.fits'
+    return get_cutout(fn, position, l, w)
+
+def get_cutout_I4(position, l, w):
+    fn = '/orange/adamginsburg/cmz/glimpse_data/GLM_00000+0000_mosaic_I4.fits'
+    return get_cutout(fn, position, l, w)
+
+def get_cutout_glimpse_rgb(position, l, w):
+    cutout_I1 = get_cutout_I1(position, l, w)
+    #cutout_I2 = get_cutout_I2(position, l, w)
+    cutout_I3 = get_cutout_I3(position, l, w)
+    cutout_I4 = get_cutout_I4(position, l, w)
+
+    rgb = np.array(
+        [
+            cutout_I4.data, # R
+            cutout_I3.data, # G 
+            cutout_I1.data, # B
+        ]
+    ).swapaxes(0,2).swapaxes(0,1)
+
+    rgb_scaled = np.array([
+        simple_norm(rgb[:,:,0], stretch='asinh', min_cut=-1, max_cut=350)(rgb[:,:,0]),
+        simple_norm(rgb[:,:,1], stretch='asinh', min_cut=-1, max_cut=200)(rgb[:,:,1]),
+        simple_norm(rgb[:,:,2], stretch='asinh', min_cut=-1, max_cut=100)(rgb[:,:,2]),
+    ]).swapaxes(0,2)
+
+    return rgb_scaled.swapaxes(0,1), cutout_I1.wcs
