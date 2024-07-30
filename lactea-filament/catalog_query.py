cbe190e296d962954a869f4e0a9546a9481d8946
Savannah Gramze <savannahgramze@c0702a-s3.ufhpc>
2024-07-30 13:40:30 -0400

    updated to remove exception catcher

--- lactea-filament/catalog_query.py
+++ lactea-filament/catalog_query.py
@@ -4,7 +4,7 @@ from astroquery.vizier import Vizier
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_catalog(catalog_name, coord, radius=1.0*u.arcmin, max_star=50):
+def get_catalog(catalog_name, coord, w=1.0*u.arcmin, l=1.0*u.arcmin):  #, max_star=50):
     """ Get Catalog
     
     Function to get catalog at a given coordinate and radius using Vizier.
@@ -18,24 +18,26 @@ def get_catalog(catalog_name, coord, radius=1.0*u.arcmin, max_star=50):
         Table: Table of the catalog.
 
     """
-    try:
-        guide = Vizier(catalog=catalog_name).query_region(coord, radius=radius)[0]
-    except:
-        print("No catalog found. Setting 2MASS catalog.")
-        guide = get_2mass_catalog(coord, radius)
+    #try:
+    Vizier.ROW_LIMIT = 5e4
+    #Vizier.query_region(coordinates=coord, width=width, height=height, catalog=['II/348/vvv2'])[0]
+    guide = Vizier.query_region(coordinates=coord, width=w, height=l, catalog=[catalog_name])[0]
+    #except:
+    #    print("No catalog found.")
+    #    return False
 
-    if len(guide) > max_star:
-        for c in guide.colnames:
-            if 'mag' in c:
-                guide = guide[~np.isnan(np.array(guide[c]))]
-        if len(guide) <= max_star:
-            return guide
-        else:
-            guide = guide[0:max_star]
+    #if len(guide) > max_star:
+    #    for c in guide.colnames:
+    #        if 'mag' in c:
+    #            guide = guide[~np.isnan(np.array(guide[c]))]
+    #    if len(guide) <= max_star:
+    #        return guide
+    #    else:
+    #        guide = guide[0:max_star]
 
     return guide
 
-def get_VVV_catalog(coord, radius=1.0*u.arcmin, max_star=50):
+def get_VVV_catalog_circ(coord, radius=1.0*u.arcmin):  #, max_star=50):
     """ Get VVV Catalog
     
     Function to get VVV catalog at a given coordinate and radius using Vizier.
@@ -48,6 +50,22 @@ def get_VVV_catalog(coord, radius=1.0*u.arcmin, max_star=50):
         Table: Table of the catalog.
 
     """
-    return get_catalog('II/376', coord, radius=radius, max_star=max_star)
+    # Vizier.query_region(coordinates=coord, width=width, height=height, catalog=['II/348/vvv2'])[0]
+    return get_catalog('II/376', coord, w=radius, l=radius)
 
+def get_VVV_catalog(coord, w=1.0*u.arcmin, l=1.0*u.arcmin):  #, max_star=50):
+    """ Get VVV Catalog
+    
+    Function to get VVV catalog at a given coordinate and radius using Vizier.
+
+    Args:
+        coord (SkyCoord): Coordinates of the center of the search.
+        radius (Quantity): Radius of the search.
+
+    Returns:
+        Table: Table of the catalog.
+
+    """
+    # Vizier.query_region(coordinates=coord, width=width, height=height, catalog=['II/348/vvv2'])[0]
+    return get_catalog('II/376', coord, w=w, l=l)
 
