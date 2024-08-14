beb183d35fb0fb4385a68f65272aa4ae42423c99
Savannah Gramze <savannahgramze@gmail.com>
2024-08-14 16:24:39 -0400

    made region and qf masks work

--- lactea-filament/jwst_plots.py
+++ lactea-filament/jwst_plots.py
@@ -8,6 +8,8 @@ from astropy.nddata import Cutout2D
 from astropy.io import fits
 from astropy.table import Table
 from cmd_plot import Plotter
+import regions
+from regions import Regions
 
 class JWSTCatalog(Plotter):
     def __init__(self, catalog):
@@ -22,7 +24,7 @@ class JWSTCatalog(Plotter):
         self.mag_ab_f410m = self.catalog['mag_ab_f410m']
         self.mag_ab_f466n = self.catalog['mag_ab_f466n']
         self.mag_ab_f187n = self.catalog['mag_ab_f187n']
-        self.mag_ab_f182n = self.catalog['mag_ab_f182m']
+        self.mag_ab_f182m = self.catalog['mag_ab_f182m']
         self.mag_ab_f212n = self.catalog['mag_ab_f212n']
 
     def color(self, band1, band2):
@@ -43,7 +45,7 @@ class JWSTCatalog(Plotter):
         mas_405 = np.logical_or(np.array(self.catalog['qfit_f405n'])<qf, np.isnan(np.array(self.mag_ab_f405n)))
         mas_410 = np.logical_or(np.array(self.catalog['qfit_f410m'])<qf, np.isnan(np.array(self.mag_ab_f410m)))
         mask = np.logical_and(mas_405, mas_410)
-        mas_466 = np.logical_or(np.array(self.catalog['qfit_f466n'])<qf, np.isnan(np.array(selfmag_ab_f466n)))
+        mas_466 = np.logical_or(np.array(self.catalog['qfit_f466n'])<qf, np.isnan(np.array(self.mag_ab_f466n)))
         mask = np.logical_and(mask, mas_466)
         mas_187 = np.logical_or(np.array(self.catalog['qfit_f187n'])<qf, np.isnan(np.array(self.mag_ab_f187n)))
         mask = np.logical_and(mask, mas_187)
@@ -53,7 +55,20 @@ class JWSTCatalog(Plotter):
         mask = np.logical_and(mask, mas_212)
         return mask
 
+    def table_qf_mask(self):
+        mask = self.get_qf_mask()
+        return self.catalog[mask]
+
     def get_count_mask(self):
         mask = np.array([self.catalog[colname] < 0.1 for colname in self.catalog.colnames if colname.startswith('emag')])
         mask = mask.max(axis=0)
         return mask
+
+    def get_region_mask(self, reg, wcs):
+        mask = reg[0].contains(self.coords, wcs=wcs)
+        #np.array([reg[0].contains(coord, wcs=wcs) for coord in self.coords])
+        return mask
+
+    def table_region_mask(self, reg, wcs):
+        mask = self.get_region_mask(reg, wcs)
+        return self.catalog[mask]
