900ce8ee75b1fea4432c4239f5e97dc44b03c058
Savannah Gramze <savannahgramze@gmail.com>
2024-08-14 16:41:56 -0400

    added mass min selection

--- lactea-filament/isochrone_plots.py
+++ lactea-filament/isochrone_plots.py
@@ -10,42 +10,39 @@ from astropy.table import Table
 import regions
 from regions import Regions
 from cmd_plot import Plotter
+from dust_extinction.averages import RRP89_MWGC, CT06_MWGC, F11_MWGC
+from dust_extinction.parameter_averages import CCM89
 
 basepath = '/home/savannahgramze/research/Research/JWST/cloudc/'
 mist = Table.read(f'{basepath}/isochrones/MIST_iso_633a08f2d8bb1.iso.cmd', 
                   header_start=12, data_start=13, format='ascii', delimiter=' ', comment='#')
 
 class Isochrone(Plotter):
-    def __init__(self, table, age, distance=8.5*u.kpc):
+    def __init__(self, table, age, distance=8.5*u.kpc, mass_min=0.96):
         super().__init__()
-        self.age_sel = table['log10_isochrone_age_yr'] == age
-        self.table = table[self.age_sel]
+        self.age = age
+        self.age_sel = table['log10_isochrone_age_yr'] == self.age
+        self.mass_min = mass_min
+        self.mass_sel = table['star_mass'] >= mass_min
+        self.sel = np.logical_and(self.age_sel, self.mass_sel)
+        self.table = table[self.sel]
         
-        self.logteff = table['log_Teff'][self.age_sel]
-        self.logg = table['log_g'][self.age_sel]
-        self.logL = table['log_L'][self.age_sel]
-        self.M_init = table['initial_mass'][self.age_sel]
-        self.M_actual = table['star_mass'][self.age_sel]
-        self.ages = table['log10_isochrone_age_yr'][self.age_sel]
-        self.Z = table['[Fe/H]'][self.age_sel]
+        self.logteff = table['log_Teff'][self.sel]
+        self.logg = table['log_g'][self.sel]
+        self.logL = table['log_L'][self.sel]
+        self.M_init = table['initial_mass'][self.sel]
+        self.M_actual = table['star_mass'][self.sel]
+        self.ages = table['log10_isochrone_age_yr'][self.sel]
+        self.Z = table['[Fe/H]'][self.sel]
         self.distance = distance
         self.distance_modulus = 5*np.log10(distance.to(u.pc).value) - 5
 
-        self.massrange = {'o': (16, np.max(self.M_init)),
-                          'b': (2.1, 16),
-                          'a': (1.4, 2.1),
-                          'f': (1.04, 1.4),
-                          'g': (0.8, 1.04),
-                          #'k': (0.45, 0.8),
-                          #'m': (0.08, 0.45)
-                          }
-
-        self.f405n = self.table['F405N'][self.age_sel]
-        self.f410m = self.table['F410M'][self.age_sel]
-        self.f466n = self.table['F466N'][self.age_sel]
-        self.f187n = self.table['F187N'][self.age_sel]
-        self.f182m = self.table['F182M'][self.age_sel]
-        self.f212n = self.table['F212N'][self.age_sel]
+        self.f405n = self.table['F405N'][self.sel]
+        self.f410m = self.table['F410M'][self.sel]
+        self.f466n = self.table['F466N'][self.sel]
+        self.f187n = self.table['F187N'][self.sel]
+        self.f182m = self.table['F182M'][self.sel]
+        self.f212n = self.table['F212N'][self.sel]
 
     def band(self, band):
         return self.table[band.upper()]
