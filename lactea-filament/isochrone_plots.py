b57da8683e3c5ef92660003143589533b6a25125
Savannah Gramze <savannahgramze@gmail.com>
2024-08-14 17:18:22 -0400

    added extinction adjustment

--- lactea-filament/isochrone_plots.py
+++ lactea-filament/isochrone_plots.py
@@ -18,14 +18,14 @@ mist = Table.read(f'{basepath}/isochrones/MIST_iso_633a08f2d8bb1.iso.cmd',
                   header_start=12, data_start=13, format='ascii', delimiter=' ', comment='#')
 
 class Isochrone(Plotter):
-    def __init__(self, table, age, distance=8.5*u.kpc, mass_min=0.96):
+    def __init__(self, table, age, distance=8.5*u.kpc, mass_min=0.05, Av=30.0):
         super().__init__()
         self.age = age
         self.age_sel = table['log10_isochrone_age_yr'] == self.age
         self.mass_min = mass_min
         self.mass_sel = table['star_mass'] >= mass_min
         self.sel = np.logical_and(self.age_sel, self.mass_sel)
-        
+
         self.table = table[self.sel]
         
         self.logteff = table['log_Teff'][self.sel]
@@ -35,15 +35,18 @@ class Isochrone(Plotter):
         self.M_actual = table['star_mass'][self.sel]
         self.ages = table['log10_isochrone_age_yr'][self.sel]
         self.Z = table['[Fe/H]'][self.sel]
+
         self.distance = distance
         self.distance_modulus = 5*np.log10(distance.to(u.pc).value) - 5
+        self.ext = CT06_MWGC()
+        self.Av = Av
 
-        self.f405n = self.table['F405N'][self.sel]
-        self.f410m = self.table['F410M'][self.sel]
-        self.f466n = self.table['F466N'][self.sel]
-        self.f187n = self.table['F187N'][self.sel]
-        self.f182m = self.table['F182M'][self.sel]
-        self.f212n = self.table['F212N'][self.sel]
+        self.f405n = self.table['F405N'] + self.distance_modulus + self.Av*self.ext(4.05*u.micron)
+        self.f410m = self.table['F410M'] + self.distance_modulus + self.Av*self.ext(4.10*u.micron)
+        self.f466n = self.table['F466N'] + self.distance_modulus + self.Av*self.ext(4.66*u.micron)
+        self.f187n = self.table['F187N'] + self.distance_modulus + self.Av*self.ext(1.87*u.micron)
+        self.f182m = self.table['F182M'] + self.distance_modulus + self.Av*self.ext(1.82*u.micron)
+        self.f212n = self.table['F212N'] + self.distance_modulus + self.Av*self.ext(2.12*u.micron)
 
     def band(self, band):
         return self.table[band.upper()]
@@ -54,8 +57,9 @@ class Isochrone(Plotter):
     def plot_isochrone(self, ax=None, **kwargs):
         if ax is None:
             ax = plt.gca()
-        ax.scatter(self.color('f187n', 'f405n'), self.f187n+self.distance_modulus, **kwargs)
+        ax.scatter(self.color('f187n', 'f405n'), self.f187n, **kwargs)
         ax.set_xlabel('F187N - F405N')
         ax.set_ylabel('F187N')
         return ax
+
         
