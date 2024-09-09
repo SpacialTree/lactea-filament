a24f3b55876906419c6d89de0ece8ac7c699b859
Savannah Gramze <savannahgramze@c0707a-s20.ufhpc>
2024-09-09 16:37:37 -0400

    extinction

--- lactea-filament/jwst_source.py
+++ lactea-filament/jwst_source.py
@@ -5,6 +5,7 @@ from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS
 from astroquery.svo_fps import SvoFps
 import cutout_manager as cm
+from dust_extinction.averages import CT06_MWGC
 
 class JWSTSource():
     def __init__(self, source, name=None):
@@ -12,6 +13,22 @@ class JWSTSource():
         self.dec = source['skycoord_ref'].dec
         self.catalog = source
         self.skycoord = SkyCoord(self.ra, self.dec, frame='icrs')
+
+    def band(self, band):
+        return self.catalog['mag_ab_'+band.lower()]
+    
+    def color(self, band1, band2):
+        return self.band(band1) - self.band(band2)
+
+    def get_Av_182410(self):
+        av182410 = (self.catalog['mag_ab_f182m'] - self.catalog['mag_ab_f410m']) / (CT06_MWGC()(1.82*u.um) - CT06_MWGC()(4.10*u.um))
+        #a182 = CT06_MWGC()(1.82*u.um)
+        #f182mdr = basetable['mag_ab_f182m'] - a182 * av182410
+        return av182410
+
+    def get_dereddened_mag(self, band='f182m'):
+        extinction = CT06_MWGC()(query_svo_fps(band)['WavelengthCen'].to(u.micron))
+        return self.catalog['mag_ab_'+band.lower()] - extinction*self.get_Av()
         
     def plot_position(self, ax=None, **kwargs):
         if ax is None:
