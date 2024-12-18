805282043b73cd6a22a51d963e9b1686a9ebbc21
Savannah Gramze <savannahgramze@gmail.com>
2024-12-18 18:56:17 -0500

    fixing reg sel

--- lactea-filament/extinction.py
+++ lactea-filament/extinction.py
@@ -145,9 +145,9 @@ def make_extinction_map(cat=cat_filament, color_cut=2.0, ext=CT06_MWGC(),
 
     mask = (cat_filament.color('f182m', 'f410m') > color_cut)
     mask = np.logical_or(mask, np.isnan(cat_filament.band('f182m')) & ~np.isnan(cat_filament.band('f410m')))
-    if reg is not None:
-        mask = np.logical_and(mask, cat_filament.table_region_mask(reg, ww))
     cat_use_red = JWSTCatalog(cat.catalog[mask])
+    if reg is not None:
+        cat_use_red = JWSTCatalog(cat_use_red.table_region_mask([reg], ww))
 
     data = get_pixcoords(cat_use_red, ww)
     seps, inds = make_kdtree(data, k=k)
